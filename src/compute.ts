4299c7b4c27bec5056a540b8ba48c6a790b5ef45
Artuur Everaert <artuur.everaert@student.hogent.be>
2020-10-05 15:59:16 +0200

    Created compute methode

--- src/compute.ts
+++ src/compute.ts
@@ -1,5 +1,50 @@
-import { Game } from "./types";
+import { Game, Frame, LastFrame } from "./types";
 
 export function compute(game: Game): number {
-  throw new Error("Not yet implemented"); // TODO
+  return game
+    .map((e) =>
+      isNormalFrame(e) ? calculateNormal(e, game) : calculateLast(e)
+    )
+    .reduce((sum: number, score: number) => sum + score, 0);
+}
+
+function isNormalFrame(frame: Frame | LastFrame): frame is Frame {
+  return frame !== undefined && frame.length == 2;
+}
+
+function calculateNormal(frame: Frame, game: Game): number {
+  if (isStrike(frame)) {
+    return calculateStrike(frame, game);
+  } else {
+    return isSpare(frame)
+      ? 10 + game[game.indexOf(frame) + 1][0]
+      : frame.reduce((sum: number, pins: number) => sum + pins, 0);
+  }
+}
+
+function calculateLast(frame: LastFrame): number {
+  return frame.reduce((sum: number, pins: number) => sum + pins, 0);
+}
+
+function isStrike(frame: Frame | LastFrame): boolean {
+  return frame[0] == 10 ? true : false;
+}
+
+function isSpare(frame: Frame | LastFrame): boolean {
+  return frame[0] + frame[1] == 10 ? true : false;
+}
+
+function calculateStrike(frame: Frame, game: Game): number {
+  if (
+    isStrike(game[game.indexOf(frame) + 1]) &&
+    isNormalFrame(game[game.indexOf(frame) + 2])
+  ) {
+    return 10 + 10 + game[game.indexOf(frame) + 2][0];
+  } else {
+    return !isNormalFrame(game[game.indexOf(frame) + 1])
+      ? 10 + game[game.indexOf(frame) + 1][0] + game[game.indexOf(frame) + 1][1]
+      : 10 +
+          game[game.indexOf(frame) + 1][0] +
+          game[game.indexOf(frame) + 2][0];
+  }
 }
--- src/server.ts
+++ src/server.ts
@@ -9,9 +9,9 @@ app.use(express.json());
 app.post("/compute", (request, response) => {
   const game = request.body.game;
   // TODO: Validate input
-
+  
   const score = compute(game);
-
+console.log(score)
   // TODO: Return response
 });
 
